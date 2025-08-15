1f4ab32638f9ca7c23b547ce25210c1730f5801c
Alvaro Souza <alvarocesar010@hotmail.com>
2025-08-15 12:24:57 +0100

    refactor: Cleaning the app page and refactor the ProductLink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,59 +2,27 @@ import Carousel from "@/components/Carousel";
 import ProductLink from "@/components/ProductLink";
 
 export default function Home() {
-  const productsLink = [
-    {
-      product: "Fechaduras Convencionais",
-      src: "/images/fechConv.webp",
-      link: "/produtos/fechaduras-convencionais",
-    },
-    {
-      product: "Fechaduras Inteligentes",
-      src: "/images/fechEle.webp",
-      link: "/produtos/fechaduras-inteligentes",
-    },
-    {
-      product: "Automatimos Portões",
-      src: "/images/motor.webp",
-      link: "/produtos/automatismos-de-portoes",
-    },
-    {
-      product: "Abertura de portas",
-      src: "/images/abertPor.webp",
-      link: "/produtos/abertura-de-portas",
-    },
-    // {
-    //   product: "Instalação, troca, ou reparação de fechaduras",
-    //   src: "/images/abertPor.webp",
-    //   link: "/produtos/servicos-de-instalacao",
-    // },
-  ];
-
   return (
-    <>
-      <main className=" flex items-center justify-center bg-gray-100">
+    <main className="flex flex-col gap-6">
+      <div className=" flex items-center justify-center bg-gray-100">
         <Carousel />
-      </main>
-      <div className="p-3 flex flex-col gap-2">
+      </div>
+
+      <div className="p-3 flex flex-col gap-6">
         <h1 className="text-3xl/tight font-semibold text-center ">
           LisLock <br /> Segurança e qualidade na prestação de serviços!
         </h1>
 
-        {/* Products list */}
-        <p className="italic text-center">
-          Conheça nossos serviços clicando nos links abaixo:
-        </p>
-        <div className="grid grid-cols-2 mx-auto gap-4">
-          {productsLink.map((item) => (
-            <ProductLink
-              product={item.product}
-              key={item.product}
-              src={item.src}
-              link={item.link}
-            />
-          ))}
-        </div>
+        <ProductLink />
       </div>
-    </>
+
+      <div className="bg-indigo-50">
+        <h2>Por que escolher a LisLock?</h2>
+        <p>Temos técnicos certificados para te atender</p>
+        <p>Preços justos e sem surpresas</p>
+        <p>Pagamento facilitado (Cartão e MB Way)</p>
+        <p>Produtos com garantia</p>
+      </div>
+    </main>
   );
 }
--- src/components/ProductLink/index.tsx
+++ src/components/ProductLink/index.tsx
@@ -1,27 +1,69 @@
 import Link from "next/link";
 import Image from "next/image";
 
-type ProductLinkProps = {
-  src: string;
+type ProductsListProps = {
   product: string;
+  src: string;
   link: string;
 };
 
-const ProductLink = ({ src, product, link }: ProductLinkProps) => {
+const productsList: ProductsListProps[] = [
+  {
+    product: "Fechaduras Convencionais",
+    src: "/images/fechConv.webp",
+    link: "/produtos/fechaduras-convencionais",
+  },
+  {
+    product: "Fechaduras Inteligentes",
+    src: "/images/fechEle.webp",
+    link: "/produtos/fechaduras-inteligentes",
+  },
+  {
+    product: "Automatimos para Portões",
+    src: "/images/motor.webp",
+    link: "/produtos/automatismos-de-portoes",
+  },
+  {
+    product: "Abertura de portas",
+    src: "/images/abertPor.webp",
+    link: "/produtos/abertura-de-portas",
+  },
+  // {
+  //   product: "Instalação, troca, ou reparação de fechaduras",
+  //   src: "/images/abertPor.webp",
+  //   link: "/produtos/servicos-de-instalacao",
+  // },
+];
+
+const ProductLink = () => {
   return (
-    <Link
-      href={link}
-      className="h-24 flex flex-row justify-center items-center gap-2 bg-indigo-100 p-2 rounded-xl"
-    >
-      {product}
-      <Image
-        className="rounded-xl object-cover"
-        width={70}
-        height={70}
-        src={src}
-        alt={product}
-      />
-    </Link>
+    <>
+      {/* Products list */}
+      <p className="italic text-center">
+        Conheça nossos serviços clicando nos links abaixo:
+      </p>
+      <div className="grid grid-cols-2 mx-auto gap-3">
+        {productsList.map((item) => (
+          <Link
+            key={item.product}
+            href={item.link}
+            className="h-24 flex flex-row justify-center items-center gap-2 bg-indigo-100 p-2 rounded-xl
+            active:bg-indigo-400 active:scale-95
+            border-1 border-indigo-600 transition duration-150
+            "
+          >
+            {item.product}
+            <Image
+              className="rounded-xl object-cover"
+              width={70}
+              height={70}
+              src={item.src}
+              alt={item.product}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
